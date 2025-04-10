4f7239ef2af8b7fa9b8fce029853c816daf247e9
FallenL3vi <seba12351212@gmail.com>
2025-04-10 14:18:06 +0200

    Update

--- main.go
+++ main.go
@@ -3,17 +3,62 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getRequests(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(`<html><body>
+	<h1>Welcome, Chirpy Admin</h1>
+	<p>Chirpy has been visited %v times!</p>
+	</body></html>`, cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) restMiddlewareMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Swap(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
+
 func main() {
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
 	server := http.Server{}
 	server.Addr =":8080"
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+
+
+	})
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	
+	mux.HandleFunc("GET /admin/metrics", cfg.getRequests)
+
+	mux.HandleFunc("POST /admin/reset", cfg.restMiddlewareMetrics)
+
 	server.Handler = mux
 
 	err := server.ListenAndServe()
 
 	if err != nil {
-		fmt.Print("ERROR")
+		fmt.Println("Server error:", err)
 	}
 }
